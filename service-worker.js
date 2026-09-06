8f6d152b44bbce92e3490ffa30c5a2a7984917de
camilo31-svg <camilo31-svg@users.noreply.github.com>
2026-09-06 21:12:04 +0200

    Force PWA update to version 2.2

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = "sj-bm-static-2026-09-06-ios-resume-3";
+const CACHE_NAME = "sj-bm-static-v2.2";
 const OFFLINE_PAGE = "./index.html";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./icons.js",
-  "./data.js",
-  "./bhajans.js",
-  "./audio-map.js",
-  "./app.js",
-  "./audio-player.js",
-  "./manifest.webmanifest",
+  "./styles.css?v=2.2",
+  "./icons.js?v=2.2",
+  "./data.js?v=2.2",
+  "./bhajans.js?v=2.2",
+  "./audio-map.js?v=2.2",
+  "./app.js?v=2.2",
+  "./audio-player.js?v=2.2",
+  "./manifest.webmanifest?v=2.2",
   "./sj-bm-icon.svg",
   "./sj-bm-icon-180.png",
   "./sj-bm-icon-192.png",
